853d340c34cbdd222fd55d9a89c6ab1ddd75e01f
RH900 <61995885+RH900@users.noreply.github.com>
2021-08-05 05:10:38 -0300

    Create ex2Main.java

---
+++ ex2Main.java
@@ -0,0 +1,36 @@
+package javareflectionlistaex2;
+
+import java.lang.reflect.*;
+
+
+public class JavaReflectionListaEx2 {
+
+   
+    public static void main(String[] args) throws Exception {
+       
+       Reflection re = new Reflection();
+       
+       
+       
+
+      
+     
+      
+      
+      re.invocaMetodos();
+      
+      
+      
+      
+      
+      
+ 
+        
+        
+        
+        
+       
+        
+    }
+    
+}
