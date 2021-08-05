a0a1b6c08b62af1df870fa5d268ec880ba5faf4c
RH900 <61995885+RH900@users.noreply.github.com>
2021-08-05 02:52:24 -0300

    Update and rename test.java to JavaReflectionListaEx1.java

---
+++ JavaReflectionListaEx1.java
@@ -0,0 +1,41 @@
+
+package javareflectionlistaex1;
+
+import java.lang.reflect.*;
+
+
+public class JavaReflectionListaEx1 {
+
+   
+    public static void main(String[] args) throws Exception {
+       
+       Reflection re = new Reflection();
+       
+       
+       
+       re.nomeClasse();
+       re.tipoClasse();
+        System.out.println("\n");
+       re.metodosClasse();
+        System.out.println("\n");
+       re.updateName();
+       
+        
+        
+//       Class<javareflectionlistaex1.Aluno> c = javareflectionlistaex1.Aluno.class;
+//       String name = c.getName();
+//        System.out.println(name);
+//        
+        
+        
+        
+        
+        
+        
+        
+        
+       
+        
+    }
+    
+}
